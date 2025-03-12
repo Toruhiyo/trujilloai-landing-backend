48f4c93c00bc8acd043300b82f4b288a8a213b99
toruhiyo <trujillo.oriol@gmail.com>
2025-03-12 13:49:39 +0100

    Removed unused wrapper.

--- src/wrappers/apryse/apryse_session.py
+++
@@ -1,8 +0,0 @@
-from apryse_sdk.PDFNetPython import PDFNet
-from src.utils.metaclasses import DynamicSingleton
-
-
-class ApryseSession(metaclass=DynamicSingleton):
-    def __init__(self, api_key: str):
-        PDFNet.Initialize(api_key)
-        PDFNet.SetDefaultDiskCachingEnabled(False)
--- src/wrappers/apryse/errors.py
+++
@@ -1,7 +0,0 @@
-class InvalidOrCorruptedFile(Exception):
-    def __init__(
-        self,
-        msg: str = "The file is invalid or corrupted",
-    ):
-        self.msg = msg
-        super().__init__(msg)
